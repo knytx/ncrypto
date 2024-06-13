140542e9ce5c6ff5ec19edf485db739e23a1d346
Naman Bishnoi <nmn@n3n.org>
2024-06-14 00:35:13 +0530

    site upd: Fri 14 Jun 2024 00:35:13

--- scripts/mizui.js
+++ scripts/mizui.js
@@ -1,38 +1,54 @@
 // Navbar Functions
-const navbar = document.getElementById("navbar");
-const navbarToggle = document.getElementById("navbar-toggle");
+const navbar = document.getElementById('navbar');
+const navbarToggle = document.getElementById('navbar-toggle');
+const main = document.getElementById('main-content');
 
-// Toggle Navbar Menu 
-navbarToggle.addEventListener("click", () => {
-  if (navbar.classList.contains("opened")) {
-    // Close Mobile Navbar
-    navbar.classList.remove("opened");
-    navbarToggle.setAttribute("aria-expanded", "false");
+function openMobileNavbar() {
+  navbar.classList.add('opened');
+  main.classList.add("no-scroll");
+  navbarToggle.setAttribute('aria-expanded', 'true');
+}
+
+function closeMobileNavbar() {
+  navbar.classList.remove('opened');
+  main.classList.remove("no-scroll");
+  navbarToggle.setAttribute('aria-expanded', 'false');
+}
+
+navbarToggle.addEventListener('click', () => {
+  if (navbar.classList.contains('opened')) {
+    closeMobileNavbar();
   } else {
-    // Open Mobile Navbar
-    navbar.classList.add("opened");
-    navbarToggle.setAttribute("aria-expanded", "true");
+    openMobileNavbar();
   }
 });
 
-// Add Background to Navbar when scrolling down below certain limit.
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 80) {
-    navbar.classList.add("navbar-bg")
-  } else {
-    navbar.classList.remove("navbar-bg")
-  }
+var dropdownButtons = document.querySelectorAll('.dropdown-btn');
+dropdownButtons.forEach(function (btn) {
+  btn.addEventListener("click", toggle.bind(this, btn.id));
 })
 
-// Alert dismissible
-const alertBar = document.getElementById("alert-closable");
-const alertClose = document.getElementById("alert-close");
+function toggle(id) {
+  var sideNav = document.querySelectorAll('.dropmenu-' + id);
+  sideNav.forEach(function (el) {
+    if (!el.classList.contains('dropmenu-open')) {
+      el.classList.add('dropmenu-open');
+    } else {
+      el.classList.remove('dropmenu-open');
+    }
+  })
+}
 
-alertClose.addEventListener("click", () => {
-  if (!alertBar.classList.contains("hide")) {
-    alertBar.classList.add("hide");
+// Alert dismissible
+const alertBar = document.getElementById('alert-closable');
+const alertClose = document.getElementById('alert-close');
+
+alertClose.addEventListener('click', () => {
+  if (!alertBar.classList.contains('hide')) {
+    alertBar.classList.add('hide');
   }
 });
+
 
 // Search close
 
@@ -40,12 +56,12 @@ let searchBox = document.querySelector(".search-box");
 
 window.onclick = function (e) {
   if (e.target == searchBox) {
-    searchBox.style.display = "none";
+    searchBox.style.display = "none"
   }
-};
+}
 
 window.onkeyup = function (e) {
   if (e.keyCode === 27) {
     searchBox.style.display = "none";
   }
-};
+}
